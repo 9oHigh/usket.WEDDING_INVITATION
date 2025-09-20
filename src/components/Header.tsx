274b73d54aa5c7d508b991a49a055af0f192658f
9oHigh <wkdska957@naver.com>
2025-09-20 21:04:52 +0900

    chore: 필요없는 파일들 제거

--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-pink-100 p-4 text-center">
-      <h1 className="text-2xl font-bold">우리 결혼식에 초대합니다 💍</h1>
-    </header>
-  );
-}
